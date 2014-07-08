8d28c824d68aa18b221bcaa086f3295c7b4902c7
David Bankier <dbankier@hotmail.com>
2014-07-08 10:08:09 +1000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*
+ * grunt-stss
+ * https://github.com/dbankier/grunt-stss
+ *
+ * Copyright (c) 2014 David Bankier
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    stss: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/test.stss']
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'stss', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/stss.js
@@ -0,0 +1,59 @@
+/*
+ * grunt-stss
+ * https://github.com/dbankier/grunt-stss
+ *
+ * Copyright (c) 2014 David Bankier
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var stss = require("stss");
+var path = require("path");
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('stss', 'grunt plugin for stss', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+      stss.renderSync({
+        data: src,
+        includePaths: [path.join(process.cwd(), path.dirname(f.src))],
+        success: function(tss) {
+          grunt.file.write(f.dest, tss);
+          grunt.log.writeln('File "' + f.dest + '" created.');
+        },
+        error: function(err) {
+          grunt.fail.fatal(err);
+        }
+      });
+    });
+  });
+};
+
